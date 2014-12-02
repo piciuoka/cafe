c536e0af126aaf6773d5466cbe0cdaa60bd05f9d
piciuoka <piciuoka@gmail.com>
2014-12-02 21:44:40 +0100

    Hello world

---
+++ Cafe/src/cafe/application/Cafe.java
@@ -0,0 +1,19 @@
+package cafe.application;
+
+import cafe.gui.MainWindow;
+
+public class Cafe {
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainWindow window = new MainWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+}
---
+++ Cafe/src/cafe/gui/MainWindow.java
@@ -0,0 +1,55 @@
+package cafe.gui;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MenuItem;
+
+public class MainWindow {
+
+	protected Shell shell;
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(600, 400);
+		shell.setText("CAFE Application");
+		
+		Menu menu = new Menu(shell, SWT.BAR);
+		shell.setMenuBar(menu);
+		
+		MenuItem mntmFile = new MenuItem(menu, SWT.CASCADE);
+		mntmFile.setText("File");
+		
+		Menu menu_1 = new Menu(mntmFile);
+		mntmFile.setMenu(menu_1);
+		
+		MenuItem mntmOpen = new MenuItem(menu_1, SWT.NONE);
+		mntmOpen.setText("Open");
+		
+		MenuItem mntmExit = new MenuItem(menu_1, SWT.NONE);
+		mntmExit.setText("Exit");
+		
+		MenuItem mntmAbout = new MenuItem(menu, SWT.NONE);
+		mntmAbout.setText("About");
+
+	}
+}
